c5733fd1a2d2bed2f1132c2b476b995f2dc7aff6
Ming Zhao <35346970+ming-zhao@users.noreply.github.com>
2018-11-11 11:27:12 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import os
 if not os.path.exists("./Modules/DesignMat"):
-  os.system("mkdir DesignMat")
+  os.path.mkdir('DesignMat')
   os.system("wget -P ./Modules/DesignMat https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/DesignMat/__init__.py -q -o /dev/null")
   os.system("wget -P ./Modules/DesignMat https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/DesignMat/gaussian.py -q -o /dev/null")
   os.system("wget -P ./Modules/DesignMat https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/DesignMat/polynomial.py -q -o /dev/null")
   os.system("wget -P ./Modules/DesignMat https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/DesignMat/sigmoidal.py -q -o /dev/null")
 if not os.path.exists("./Modules/Regressor"):
-  os.system("mkdir Regressor")
+  os.path.mkdir('Regressor')
   os.system("wget -P ./Modules/Regressor https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Regressor/__init__.py -q -o /dev/null")
   os.system("wget -P ./Modules/Regressor https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Regressor/bayesian.py -q -o /dev/null")
   os.system("wget -P ./Modules/Regressor https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Regressor/empirical.py -q -o /dev/null")
   os.system("wget -P ./Modules/Regressor https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Regressor/least_squares.py -q -o /dev/null")
   os.system("wget -P ./Modules/Regressor https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Regressor/Regressor.py -q -o /dev/null")
 if not os.path.exists("./Modules/Classifier"):
-  os.system("mkdir Classifier")
+  os.path.mkdir('Classifier')
   os.system("wget -P ./Modules/Classifier https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Classifier/__init__.py -q -o /dev/null")
   os.system("wget -P ./Modules/Classifier https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Classifier/logistic.py -q -o /dev/null")
   os.system("wget -P ./Modules/Classifier https://raw.githubusercontent.com/ming-zhao/Intro-to-Deep-Learning/master/Modules/Classifier/least_squares.py -q -o /dev/null")
